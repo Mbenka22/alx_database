a2f42b1e2f3002c2e64b4af2c49c4707c7a96c3c
Mbenka22 <mbenka22@gmail.com>
2024-01-10 01:31:39 +0300

    INSERT

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,3 +1,4 @@
 -- script that inserts a new row in the table first_table 
 USE database hbtn_0c_0;
-INSERT INTO first_table(Id,name)VALUES(89,'Holberton School');
+INSERT INTO first_table(Id,name)
+VALUES(89,'Holberton School');
