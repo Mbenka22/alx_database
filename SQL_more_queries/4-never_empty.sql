60f656b5cc40f0a8acd992dbce62a18f241cebd0
Mbenka22 <mbenka22@gmail.com>
2024-01-11 00:41:34 +0300

    not null

--- SQL_more_queries/4-never_empty.sql
+++ SQL_more_queries/4-never_empty.sql
@@ -1,2 +1,3 @@
 -- id  not NULL
-CREATE A TABLE IF NOT EXISTS id_not_null(id INT DEFAULT 1, name VARCHAR(256) );
+CREATE A TABLE IF NOT EXISTS id_not_null(
+    id INT DEFAULT 1,name VARCHAR(256) );
