c6791f6eb89db84c0cf687a524d38f7d356d9f37
Mbenka22 <mbenka22@gmail.com>
2024-01-11 00:29:48 +0300

    not null

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,3 +1,3 @@
 -- Write a script that creates the table force_name on your MySQL server. 
 CREATE TABLE IF NOT EXISTS force_name(
-    Id, name VARCHAR (256)NOT NULL);
+    Id INT, name VARCHAR (256)NOT NULL);
