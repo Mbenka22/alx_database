ce0e806fe57f37701d2f9150a69cc7e380a76595
Mbenka22 <mbenka22@gmail.com>
2024-01-11 02:07:09 +0300

    inner join

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- list cities
 SELECT cities.id, cities.name, states.name
 FROM hbtn_0d_usa.cities
-JOIN hbtn_0d_usa.states ON cities.state_id = states.id
+INNER JOIN hbtn_0d_usa.states ON cities.state_id = states.id
 ORDER BY cities.id ASC;
