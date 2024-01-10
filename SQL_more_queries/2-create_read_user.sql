95be63af2c65d570b8af1f99a44d0620636a5653
Mbenka22 <mbenka22@gmail.com>
2024-01-11 00:08:06 +0300

    privy

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -11,4 +11,4 @@ CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
 
 -- Flush privileges to apply changes
-FLUSH PRIVILEGES;
+-- FLUSH PRIVILEGES;
