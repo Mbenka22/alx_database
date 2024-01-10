6698324c4b735020924c9da9731b6610404f2733
Mbenka22 <mbenka22@gmail.com>
2024-01-10 23:36:28 +0300

    grants

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,5 +1,5 @@
 -- giving privileges to new users
-CREATE USER 'user_0d_1'@'localhost';
+-- CREATE USER 'user_0d_1'@'localhost';
 
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
