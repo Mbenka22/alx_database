643e72ae5e17fd2c4a021e761c18a17bc54cc60c
Mbenka22 <mbenka22@gmail.com>
2024-01-11 01:05:46 +0300

    sTATES

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,7 +1,6 @@
 -- script that creates the database and a TABLE
 CREATE DATABASE hbtn_0d_usa;
-USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS states(
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(
     id INT UNIQUE NOT NULL PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(256) NOT NULL
 );
