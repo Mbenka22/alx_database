282c693516d59e34771e0567f21b13ca15bb011b
Mbenka22 <mbenka22@gmail.com>
2024-01-11 12:41:10 +0300

    new

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,3 +1,4 @@
+-- Active: 1704828299617@@127.0.0.1@3006@hbtn_0d_usa
 -- script that creates the database hbtn_0d_usa and the table cities
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
